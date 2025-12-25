775731ca45f3b94faeea248cca8fbac484e643c4
luqi-png <ranaluqman2003@gmail.com>
2025-12-25 21:50:20 +0100

    Add files via upload

---
+++ src/nlp/description_generator.py
@@ -0,0 +1,5 @@
+def generate_description(tags):
+    """
+    Generates a dummy product description based on tags.
+    """
+    return f"This is a {', '.join(tags)} outfit suitable for everyday wear."
